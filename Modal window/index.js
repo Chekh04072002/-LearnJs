0c0898a9d1d6367aab79bacb19a274ac01c28274
Sasha <chekh-sasha3@mail.ru>
2022-08-22 03:38:44 +0300

    added Modal window

---
+++ Modal window/index.js
@@ -0,0 +1,11 @@
+const myModal = $.modal({
+    title: 'My Modal Window',
+    closable: true,
+    content: `<h4>My Content in modal Window</h4>
+              <p>Lorem ipsum dolor sit amet.</p>`,
+    width: '400px'
+});
+
+
+
+
